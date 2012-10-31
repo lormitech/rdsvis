33ed0aee303f40dcb3d2dc7df895f1e98527a514
Lorenzo Milazzo <lormitech@googlemail.com>
2012-10-31 12:18:41 +0000

    update 121031

--- src/rdsvis.js
+++ src/rdsvis.js
@@ -1,7 +1,7 @@
 
 //
-// version:       0.2.0
-// last modified: 14.09.12
+// version:       0.3.0
+// last modified: 31.10.12
 //
 //
 //          ++  RDSvis  ++
@@ -19,194 +19,6 @@
 
 
 //--
-
-//
-// -- RDS tree ---
-//
-// note that ...
-//   - each node corresponds to a participant;
-//   - each partecipant can be identified by one of these values:
-//       [id1] = the respondent ID; value in the first column of the RDS data set;
-//       [id2] = the position, i.e. row number, in the original RDS data set
-//   - a participant can be either a seed or a respondent;
-//     for a seed, the respondent ID (id1) is equal to -1;
-//
-
-//
-// a JSON tree structure is a hash (i.e. {});
-// it consists of nodes, each having as properties:
-//
-//   id      [string] = unique identifier for the node
-//   name    [string] = name of the node
-//   data    [object] = optional property containing a hash (i.e. {}) where
-//                      it is possible to store info/data about the node
-//   children [array] = optional property containing an array (i.e. []) where
-//                      it is possible to store the children of the node
-//
-// http://thejit.org/static/v20/Docs/files/Core/Core-js.html
-//
-  
-
-// constructor for a tree in JSON format
-function tree_json(id, name, data, children) {
-
-  // `id' coincides with `id2'
-  this.id = id;
-  // `name' coincides with `id1'
-  this.name = name;
-  this.data = data;
-  this.children = children;
-  this.addChild = function(new_child) {
-    this.children.push(new_child);
-  }  
-
-}
-
-
-// function to perform the concatenation test
-// (recursive function)
-function concatenationTest(stree, stree_ref) {
-	
-  var concat_out = new Array();
-  // initiliaze the data structure `concat_out':
-  // if the concatenation test fails, the function
-  // returns the original subtree
-  concat_out.push(stree);
-  concat_out.push(0);		  
-  var concat_out_c;
- 
-  // if the subtree has children, perform the
-  // concatenation test on them ...
-  if (stree.children != 0) {
-    for (var k=0; k<stree.children.length; k++) {	
-      concat_out_c = concatenationTest(stree.children[k], stree_ref);
-      if (concat_out_c[1] == 1) {
-        stree.children[k] = concat_out_c[0];
-        concat_out[0] = stree; 
-        concat_out[1] = concat_out_c[1];
-        return(concat_out);
-      }
-    }
-  }
- 
-  // concatenation test
-  if (stree.id == stree_ref.id) {
-    for (var k=0; k<stree_ref.children.length; k++) {
-      stree.addChild(stree_ref.children[k]);
-    }	
-    concat_out[0] = stree;
-    concat_out[1] = 1;	   
-    return(concat_out);
-  }
-	 
-  return(concat_out);
-
-}
-
-
-// function to concatenate the subtrees
-function concatenateJSONSubTrees(strees_json) {
-
-  // note that, in order to optimize the algorithm, the array
-  // `strees_json' is shrinked and as a result the number of subtrees
-  // to process is reduced at each concatenation step
-
-  var strees_json_t = strees_json.slice();
-  var concat_out;
-  var sc = 0;
-	
-  for (var i=0; i<strees_json_t.length; i++) {
-    for (var j=0; j<strees_json.length; j++) {
-      for (var k=0; k<strees_json[j].children.length; k++) {
-        // concatenation test
-	concat_out = concatenationTest(strees_json[j].children[k], strees_json_t[i]);
-        if (concat_out[1] == 1) { 		
-          strees_json[j].children[k] = concat_out[0];
-          // shrinking the array
-          strees_json.splice((i - sc), 1);
-          sc = sc + 1;  
-          break;
-        }
-      }
-      if (concat_out[1] == 1) break;
-    }
-  }
-  
-  return(strees_json);
-  
-}
-
-
-// function to generate the subtrees
-function generateJSONSubTrees(node_pairs, rds_data) {
-
-  var idx_p, idx_c, no_strees_c, idx_t, start;
-  var strees_json = new Array();
-  
-  var pa = getParents(node_pairs);
-  pa = pa.sort(function(a,b){return a-b});
-  pa_u = pa.unique();
-   
-  for (var ip=0; ip<pa_u.length; ip++) {
-
-    var idxs_c = new Array();
-    idx_p = 0;
-    idx_c = 0;
-    no_strees_c = 0;
-    idx_t = 0;
-    start = 0;
-    while (idx_t != -1) {
-      idx_t = node_pairs.indexOf(pa_u[ip], start);
-      if ((idx_t != -1) && (idx_t%2 == 0)) {
-        idx_p = idx_t;
-        start = idx_t + 1;
-        idx_c = idx_p + 1;
-        idxs_c.push(idx_c);
-        no_strees_c = no_strees_c + 1;
-      }
-      if (idx_t%2 != 0) start = idx_t + 1;
-    }
- 	   	
-    // parent
-    var stree_p = {};
-    tree_json.call(stree_p, node_pairs[idx_p], rds_data[node_pairs[idx_p]][0], {}, []);
-    // children
-    for (var ic=0; ic<no_strees_c; ic++) {
-      var stree_c = {};
-      tree_json.call(stree_c, node_pairs[idxs_c[ic]], rds_data[node_pairs[idxs_c[ic]]][0], {}, []);
-      stree_p.addChild(stree_c);
-    }
-    strees_json.push(stree_p);
-	
-  }
-
-  return(strees_json);
-  
-}
-
-
-// function to generate the tree
-function generateJSONTree(node_pairs, rds_data) {
-  
-  var strees_json = generateJSONSubTrees(node_pairs, rds_data);  
-  var tree_json = concatenateJSONSubTrees(strees_json);
-     
-  return(tree_json);
-  
-}
-
-
-// function to get the parents for each node pairs
-function getParents(node_pairs) {
-
-  var parents = Array();
-  for (var i=0; i<node_pairs.length; i++) {
-    if (i%2==0) parents.push(node_pairs[i]);  
-  }
-  
-  return(parents);
-
-}
 
 
 // function to get the nodes
@@ -246,11 +58,11 @@ function getNodePairs(id_cfromrecr, id_ctoresp, no_rows, no_cols) {
 }
 
 
-// main function
-function processData(document){
+// function to generate the RDS tree
+function generateRDSTree(){
  
   // I/O processing
-  indata = fromInput(document) ;
+  indata = fromInput() ;
  
   // RDS data (2D array)
   var rds_data = indata[0];
